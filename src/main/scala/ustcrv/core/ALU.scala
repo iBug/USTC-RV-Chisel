0e97d2e47013b842dccf1a5675ee648d7309998d
iBug <7273074+iBug@users.noreply.github.com>
2019-04-16 15:35:21 +0800

    extend ALU control signal to 4 bits

--- src/main/scala/ustcrv/core/ALU.scala
+++ src/main/scala/ustcrv/core/ALU.scala
@@ -4,14 +4,14 @@ import chisel3._
 import chisel3.util._
 
 object ALU {
-  val ADD  = 0.U(3.W)
-  val SUB  = 0.U(3.W)
-  val SLL  = 1.U(3.W)
-  val SLT  = 2.U(3.W)
-  val SLTU = 3.U(3.W)
-  val XOR  = 4.U(3.W)
-  val SRL  = 5.U(3.W)
-  val SRA  = 5.U(3.W)
-  val OR   = 6.U(3.W)
-  val AND  = 7.U(3.W)
+  val ADD  = "b0000".U(4.W)
+  val SUB  = "b1000".U(4.W)
+  val SLL  = "b0001".U(4.W)
+  val SLT  = "b0010".U(4.W)
+  val SLTU = "b0011".U(4.W)
+  val XOR  = "b0100".U(4.W)
+  val SRL  = "b0101".U(4.W)
+  val SRA  = "b1101".U(4.W)
+  val OR   = "b0110".U(4.W)
+  val AND  = "b1111".U(4.W)
 }
