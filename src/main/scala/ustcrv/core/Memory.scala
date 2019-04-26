24e309c57a31aeb1346381720841b7cd4065e87b
iBug <7273074+iBug@users.noreply.github.com>
2019-04-26 20:38:37 +0800

    Must support offsets for now

--- src/main/scala/ustcrv/core/Memory.scala
+++ src/main/scala/ustcrv/core/Memory.scala
@@ -3,18 +3,19 @@ package ustcrv.core
 import chisel3._
 import chisel3.util._
 
-class IMem(val size: Int = 4096) extends Module {
+class IMem(val size: Int = 4096, val offset: Int = 0) extends Module {
   val io = IO(new Bundle {
     val in = Input(UInt(32.W))
     val out = Output(UInt(32.W))
   })
 
   val mem = SyncReadMem(size, UInt(32.W))
+  val addr = (io.in - offset.U) >> 2
 
-  io.out := mem.read(io.in)
+  io.out := mem.read(addr)
 }
 
-class DMem(val size: Int = 4096) extends Module {
+class DMem(val size: Int = 4096, val offset: Int = 0) extends Module {
   val io = IO(new Bundle {
     val addr = Input(UInt(32.W))
     val dataR = Output(UInt(32.W))
@@ -23,7 +24,7 @@ class DMem(val size: Int = 4096) extends Module {
   })
 
   val mem = SyncReadMem(size, UInt(32.W))
-  val addr = io.addr >> 2 // access unit is 4 bytes
+  val addr = (io.addr - offset.U) >> 2 // access unit is 4 bytes
 
   when (io.memRW) {
     mem.write(addr, io.dataW)
