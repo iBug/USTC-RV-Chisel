c0cea15f5761eaec78b8699464f52117d6461204
iBug <7273074+iBug@users.noreply.github.com>
2019-04-26 21:05:55 +0800

    Add debug support for IMem and DMem

--- src/main/scala/ustcrv/core/Memory.scala
+++ src/main/scala/ustcrv/core/Memory.scala
@@ -3,33 +3,65 @@ package ustcrv.core
 import chisel3._
 import chisel3.util._
 
-class IMem(val size: Int = 4096, val offset: Int = 0) extends Module {
+class IMem(val size: Int = 4096, val offset: Int = 0, val debug: Boolean = false) extends Module {
   val io = IO(new Bundle {
     val in = Input(UInt(32.W))
     val out = Output(UInt(32.W))
+    if (debug) {
+      val drAddr = Input(UInt(32.W))
+      val dwAddr = Input(UInt(32.W))
+      val drData = Output(UInt(32.W))
+      val dwData = Input(UInt(32.W))
+      val dMode = Input(Bool()) // 0 = read, 1 = write
+    }
   })
 
   val mem = SyncReadMem(size, UInt(32.W))
-  val addr = (io.in - offset.U) >> 2
+  val addr = (io.in - offset.U) >> 2.U
 
   io.out := mem.read(addr)
+
+  if (debug) {
+    when (io.dMode) {
+      mem.write(io.dwAddr >> 2.U, io.dwData)
+      io.drData := DontCare
+    } .otherwise {
+      io.drData := mem.read(io.drAddr)
+    }
+  }
 }
 
-class DMem(val size: Int = 4096, val offset: Int = 0) extends Module {
+class DMem(val size: Int = 4096, val offset: Int = 0, val debug: Boolean = false) extends Module {
   val io = IO(new Bundle {
     val addr = Input(UInt(32.W))
     val dataR = Output(UInt(32.W))
     val dataW = Input(UInt(32.W))
     val memRW = Input(Bool())
+    if (debug) {
+      val drAddr = Input(UInt(32.W))
+      val dwAddr = Input(UInt(32.W))
+      val drData = Output(UInt(32.W))
+      val dwData = Input(UInt(32.W))
+      val dMode = Input(Bool()) // 0 = read, 1 = write
+    }
   })
 
   val mem = SyncReadMem(size, UInt(32.W))
-  val addr = (io.addr - offset.U) >> 2 // access unit is 4 bytes
+  val addr = (io.addr - offset.U) >> 2.U // access unit is 4 bytes
 
   when (io.memRW) {
     mem.write(addr, io.dataW)
     io.dataR := DontCare
   } .otherwise {
     io.dataR := mem.read(addr)
+  }
+
+  if (debug) {
+    when (io.dMode) {
+      mem.write(io.dwAddr >> 2.U, io.dwData)
+      io.drData := DontCare
+    } .otherwise {
+      io.drData := mem.read(io.drAddr)
+    }
   }
 }
