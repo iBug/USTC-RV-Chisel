9f926a42560b8bfe329a9d369e588c5449bc3571
iBug <iBug@users.noreply.github.com>
2019-05-22 22:35:46 +0800

    Find out how to load data

--- src/test/scala/ustcrv/core/PackageTester.scala
+++ src/test/scala/ustcrv/core/PackageTester.scala
@@ -4,8 +4,14 @@ import chisel3._
 import chisel3.util._
 import chisel3.iotesters._
 
-import java.nio.file.{Files, Paths}
+import scala.io.Source
 
 class PackageTester(val c: Package) extends PeekPokeTester(c) {
   // Data Location: (pwd)/src/test/data
+
+  // Load data
+  val iData = Soruce.fromFile("src/test/data/riscv.imem").getLines.map(BigInt(s, 16)).toList
+  val dData = Soruce.fromFile("src/test/data/riscv.dmem").getLines.map(BigInt(s, 16)).toList
+
+  // Merge
 }
