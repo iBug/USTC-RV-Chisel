5c37930b552cc9cb3ddca5181007bd73f170527a
iBug <iBug@users.noreply.github.com>
2019-05-25 17:02:09 +0800

    Blatant typo

--- src/test/scala/ustcrv/core/PackageTester.scala
+++ src/test/scala/ustcrv/core/PackageTester.scala
@@ -10,8 +10,8 @@ class PackageTester(val c: Package) extends PeekPokeTester(c) {
   // Data Location: (pwd)/src/test/data
 
   // Load data
-  val iData = Soruce.fromFile("src/test/data/riscv.imem").getLines.map(BigInt(s, 16)).toList
-  val dData = Soruce.fromFile("src/test/data/riscv.dmem").getLines.map(BigInt(s, 16)).toList
+  val iData = Source.fromFile("src/test/data/riscv.imem").getLines.map(BigInt(s, 16)).toList
+  val dData = Source.fromFile("src/test/data/riscv.dmem").getLines.map(BigInt(s, 16)).toList
 
   // Merge
 }
