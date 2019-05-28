0a86040a69ddff72a0e73816c0d23c27a0ecc970
iBug <git@ibugone.com>
2019-05-28 20:18:58 +0800

    Merge util into one file

--- src/main/scala/ustcrv/util/Util.scala
+++ src/main/scala/ustcrv/util/Util.scala
@@ -5,3 +5,7 @@ import chisel3._
 object OptionalPort {
   def apply(condition: Boolean, port: Any): Any = (if (condition) Some(port) else None) get
 }
+
+object PosEdge {
+  def apply(signal: Bool): Bool = signal && !RegNext(signal)
+}
