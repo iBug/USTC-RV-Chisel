a9bdb476eb8dbf8d445395814544ac3fcb43c44c
iBug <git@ibugone.com>
2019-05-28 21:11:40 +0800

    Add XDC constraint file

--- src/verilog/MemRom.v
+++ src/verilog/MemRom.v
@@ -2,7 +2,8 @@ module IMemROM (
     input [7:0] addr,
     output [31:0] data
 );
-    reg [31:0] rom [255:0];
+    reg [31:0] rom [0:255];
+    assign data = rom[addr];
 
     initial
         $readmemh("USTC-RV-Chisel.srcs/sources_1/new/data/riscv.imem");
@@ -12,7 +13,8 @@ module DMemROM (
     input [7:0] addr,
     output [31:0] data
 );
-    reg [31:0] rom [255:0];
+    reg [31:0] rom [0:255];
+    assign data = rom[addr];
 
     initial
         $readmemh("USTC-RV-Chisel.srcs/sources_1/new/data/riscv.dmem");
