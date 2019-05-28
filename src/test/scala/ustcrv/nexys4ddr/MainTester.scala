19a6ba897b9baa9f38006b261a3593f1a9a7543b
iBug <git@ibugone.com>
2019-05-29 01:08:15 +0800

    Initialize tester for the FPGA package

---
+++ src/test/scala/ustcrv/nexys4ddr/MainTester.scala
@@ -0,0 +1,28 @@
+package ustcrv.nexys4ddr
+
+import chisel3._
+import chisel3.util._
+import chisel3.iotesters._
+
+import org.scalatest.{Matchers, FlatSpec}
+
+class MainSpec extends FlatSpec with Matchers {
+  behavior of "MainSpec"
+
+  it should "work" in {
+    chisel3.iotesters.Driver(() => new Main) {
+      c => new MainTester(c)
+    } should be(true)
+  }
+}
+
+object MainTests extends App {
+  val a = Array("--display-base", "16")
+  chisel3.iotesters.Driver.execute(a, () => new Main) {
+    c => new MainTester(c)
+  }
+}
+
+class MainTester(val c: Main) extends PeekPokeTester(c) {
+  // TBA
+}
