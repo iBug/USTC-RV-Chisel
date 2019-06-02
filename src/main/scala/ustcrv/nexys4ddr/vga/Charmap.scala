d77b2ff9d7a7294e045c6cb4bd60214c55110f0c
iBug <git@ibugone.com>
2019-06-02 23:44:30 +0800

    Add character mapper

---
+++ src/main/scala/ustcrv/nexys4ddr/vga/Charmap.scala
@@ -0,0 +1,20 @@
+package ustcrv.nexys4ddr.vga
+
+import chisel3._
+import chisel3.util._
+
+import scala.io.Source
+
+class Charmap extends Module {
+  val io = IO(new Bundle {
+    val in = Input(UInt(8.W))
+    val out = Output(Vec(16, UInt(8.W)))
+  })
+
+  val data = Source.fromFile("src/data/pixel_text.dat").getLines.map(
+      l => VecInit(
+        l split(" ") map(s => ("h" + s).U(8.W)) toList
+      )) toList
+  val rom = VecInit(data)
+  io.out := Mux(io.in < 0x20.U || io.in >= 0x7F.U, 0.U(8.W), rom(0x20.U + io.in))
+}
