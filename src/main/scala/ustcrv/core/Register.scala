19678526f9a4b389b560644a89d3359b3df34dad
iBug <git@ibugone.com>
2020-07-28 16:16:48 +0800

    CA Pipeline RegisterFile

--- src/main/scala/ustcrv/core/Register.scala
+++ src/main/scala/ustcrv/core/Register.scala
@@ -3,24 +3,36 @@ package ustcrv.core
 import chisel3._
 import chisel3.util._
 
-class RegisterFile(val wData: Int = 32, val wAddr: Int = 5) extends Module {
-  val io = IO(new Bundle {
-    val addrA = Input(UInt(wAddr.W))
-    val addrB = Input(UInt(wAddr.W))
-    val dataA = Output(UInt(wData.W))
-    val dataB = Output(UInt(wData.W))
-    val addrD = Input(UInt(wAddr.W))
-    val dataD = Input(UInt(wData.W))
-    val regWEn = Input(Bool())
-  })
+class RegisterFileIO extends Bundle {
+  val addrA = Input(UInt(wAddr.W))
+  val addrB = Input(UInt(wAddr.W))
+  val dataA = Output(UInt(wData.W))
+  val dataB = Output(UInt(wData.W))
+  val addrD = Input(UInt(wAddr.W))
+  val dataD = Input(UInt(wData.W))
+  val regWEn = Input(Bool())
+}
+
+class RegisterFileInternal(val wData: Int = 32, val wAddr: Int = 5) extends Module {
+  val io = IO(new RegisterFileIO)
 
   val regCount = 1 << wAddr // Number of registers
   val r = RegInit(VecInit(Seq.fill(regCount)(0.U(wData.W))))
 
-  io.dataA := r(io.addrA)
-  io.dataB := r(io.addrB)
+  // x0 should be always zero
+  io.dataA := Mux(io.addrA === 0.U, 0.U(wData.W), r(io.addrA))
+  io.dataB := Mux(io.addrB === 0.U, 0.U(wData.W), r(io.addrB))
 
-  when (io.regWEn && io.addrD =/= 0.U) {  // x0 should be always zero
+  when (io.regWEn) {
     r(io.addrD) := io.dataD
+  }
+}
+
+// This wrapper is used for inverting clock signal (posedge -> negedge)
+class RegisterFile(val wData: Int = 32, val wAddr: Int = 5) extends Module {
+  val io = IO(new RegisterFileIO)
+  withClock(~clock) {
+    val internal = Module(new RegisterFileInternal)
+    internal.io <> io
   }
 }
